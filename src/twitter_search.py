3f9fd44017b17ac470478715197bb941fbcb6b6f
marijanedjalkova <nedyalkova.marya@gmail.com>
2017-02-26 20:37:59 +0000

    twitter search working

--- src/twitter_search.py
+++ src/twitter_search.py
@@ -1,7 +1,19 @@
 import tweepy
+import json
 
 def main():
-	print "hello world"
+	with open("auth_twitter.txt") as f:
+		d = json.load(f)
+
+	consumer_key = d['consumer_key']
+
+	auth = tweepy.OAuthHandler(d["consumer_key"], d["consumer_secret"])
+	auth.set_access_token(d["access_token"], d["access_token_secret"])
+	api = tweepy.API(auth)
+	results = api.search(q="Mice")
+	for result in results:
+		print result.text
+
 
 
 if __name__ == '__main__':
