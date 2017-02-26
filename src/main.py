99f9084f82c0e27d17b7ed582a14b435e0e5aaf2
marijanedjalkova <nedyalkova.marya@gmail.com>
2017-02-26 23:12:16 +0000

    extracted twitter stuff into a separate file

--- src/main.py
+++ src/main.py
@@ -1,32 +1,11 @@
-import tweepy
-import json
-
-def get_twitter_api():
-	with open("auth_twitter.txt") as f:
-		d = json.load(f)
-
-	auth = tweepy.OAuthHandler(d["consumer_key"], d["consumer_secret"])
-	auth.set_access_token(d["access_token"], d["access_token_secret"])
-	return tweepy.API(auth, wait_on_rate_limit=True)
-
-def search(api, query, n):
-	tweets = tweet_batch = api.search(q=query, count=n)
-	ct = 1
-	while len(tweets) < n and ct < 100:
-		print "found ", (len(tweets))
-		tweet_batch = api.search(q=query, 
-								 count=n - len(tweets),
-								 max_id=tweet_batch.max_id)
-		tweets.extend(tweet_batch)
-		ct += 1
-	return tweets
+from twitter_tool import TwitterTool
 
 def main():
-	api = get_twitter_api()
+	tt = TwitterTool()
 
-	tweets = search(api, "standrews", 12)
+	tweets = tt.search_tweets("standrews", 12)
 	for tweet in tweets:
-		print tweet.created_at, '\n',  tweet.text, '\n\n'
+		print tweet.created_at, '\n',  tweet.text
 		print "--------------"
 
 
