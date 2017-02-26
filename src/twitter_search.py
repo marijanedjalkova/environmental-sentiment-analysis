f3bb154b1f8abce66c402b4741527736b0a8c5e5
marijanedjalkova <nedyalkova.marya@gmail.com>
2017-02-26 21:12:10 +0000

    search works for any(?) number of tweets

--- src/twitter_search.py
+++ src/twitter_search.py
@@ -1,19 +1,33 @@
 import tweepy
 import json
 
-def main():
+def get_twitter_api():
 	with open("auth_twitter.txt") as f:
 		d = json.load(f)
 
-	consumer_key = d['consumer_key']
-
 	auth = tweepy.OAuthHandler(d["consumer_key"], d["consumer_secret"])
 	auth.set_access_token(d["access_token"], d["access_token_secret"])
-	api = tweepy.API(auth)
-	results = api.search(q="Mice")
-	for result in results:
-		print result.text
+	return tweepy.API(auth, wait_on_rate_limit=True)
 
+def search(api, query, n):
+	tweets = tweet_batch = api.search(q=query, count=n)
+	ct = 1
+	while len(tweets) < n and ct < 100:
+		print "found ", (len(tweets))
+		tweet_batch = api.search(q=query, 
+								 count=n - len(tweets),
+								 max_id=tweet_batch.max_id)
+		tweets.extend(tweet_batch)
+		ct += 1
+	return tweets
+
+def main():
+	api = get_twitter_api()
+
+	tweets = search(api, "standrews", 12)
+	for tweet in tweets:
+		print tweet.created_at, '\n',  tweet.text, '\n\n'
+		print "--------------"
 
 
 if __name__ == '__main__':
