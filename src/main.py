c284ed4efe940863cb1d84bea73860b602e3a4f7
marijanedjalkova <nedyalkova.marya@gmail.com>
2017-02-27 23:45:29 +0000

    started training. Accessed data and form BoW on it

--- src/main.py
+++ src/main.py
@@ -1,6 +1,8 @@
 from twitter_tool import TwitterTool
+import csv
+from nltk import word_tokenize
 
-def main():
+def test_twitter():
 	tt = TwitterTool()
 	tt.stream(["london"])
 	return
@@ -8,6 +10,23 @@ def main():
 	for tweet in tweets:
 		print tweet.created_at, '\n',  tweet.text
 		print "--------------"
+
+def get_bag_of_words(string):
+	return dict([(word, True) for word in word_tokenize(string)])
+
+def main():
+	with open("../resources/Sentiment-Analysis-Dataset.csv") as csvfile:
+		data = csv.reader(csvfile) 
+		# format ItemID, Sentiment, SentimentSource, SentimentText
+		# SentimentSource set is ['Sentiment140', 'Kaggle'] - not sure if it matters
+
+		for row in data:
+			text = row[3]
+			wordset = get_bag_of_words(text)
+			polarity = row[1]	
+
+	
+
 
 if __name__ == '__main__':
 	main()
