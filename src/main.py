158d8f8ef3f01e9fe5f6fff29213dfd1d960d5cb
marijanedjalkova <nedyalkova.marya@gmail.com>
2017-02-28 18:43:01 +0000

    extracted it to a separate class

--- src/main.py
+++ src/main.py
@@ -1,8 +1,8 @@
 from twitter_tool import TwitterTool
-import csv
-from nltk import word_tokenize
 
-dist = {}
+from very_naive_classifier import SimpleClassifier
+
+
 
 def test_twitter():
 	tt = TwitterTool()
@@ -13,68 +13,11 @@ def test_twitter():
 		print tweet.created_at, '\n',  tweet.text
 		print "--------------"
 
-def get_bag_of_words(string):
-	words = []
-	try:
-		words = word_tokenize(string)
-	except UnicodeDecodeError:
-		pass
-	return dict([(word, True) for word in words])
-
-def get_polarity(text):
-	res = 0
-	try:
-		words = word_tokenize(text)
-		for w in words:
-			res += dist[w] if w in dist else 0
-	except UnicodeDecodeError:
-		pass
-	if res < 0:
-		return 0 
-	return 1
 
 def main():
-	
-	dataset_len = 1578615 
-	test_limit = dataset_len * 2 /3 / 10 # TODO remove division by 10
-	with open("../resources/Sentiment-Analysis-Dataset.csv") as csvfile:
-		data = csv.reader(csvfile) # 1578615 
-		next(data, None) # skip headers
-		# format ItemID, Sentiment, SentimentSource, SentimentText
-		# SentimentSource set is ['Sentiment140', 'Kaggle'] - not sure if it matters
-		counter = 0
-		total = 0
-		for row in data:
-			
-			index = int(row[0])
-			if index % 10000 == 0:
-				print index
-			text = row[3]
-			polarity = int(row[1])
-			polarity_index = polarity if polarity==1 else -1
-			if index < test_limit:
-				try:
-					words = word_tokenize(text)
-					for w in words:
-						if w in dist:
-							dist[w] += polarity_index
-						else: dist[w] = polarity_index
-				except UnicodeDecodeError:
-					pass
-			else:
-				
-				counter += 1
-				if counter < 1000:
-					predicted = get_polarity(text)
-					if predicted == polarity:
-						total += 1
-					continue
-				print total/10 , "%"
-				return
+	s = SimpleClassifier()
+	s.train_and_test()
 
-				
-
-	
 
 
 if __name__ == '__main__':
---
+++ src/very_naive_classifier.py
@@ -0,0 +1,67 @@
+from nltk import word_tokenize
+import csv
+
+class SimpleClassifier:
+
+	def __init__(self):
+		self.dist = {}
+
+	def get_bag_of_words(self, string):
+		words = []
+		try:
+			words = word_tokenize(string)
+		except UnicodeDecodeError:
+			pass
+		return dict([(word, True) for word in words])
+
+	def get_polarity(self, text):
+		res = 0
+		try:
+			words = word_tokenize(text)
+			for w in words:
+				res += self.dist[w] if w in self.dist else 0
+		except UnicodeDecodeError:
+			pass
+		if res <= 0:
+			return 0 
+		return 1
+
+	def train_and_test(self):
+		dataset_len = 1578615 
+		train_limit = dataset_len * 2 /3 / 50 # TODO remove division by 10
+		test_limit = train_limit + 1000
+		print "train limit", train_limit, ", test limit", test_limit
+		with open("/cs/home/mn39/Documents/MSciDissertation/resources/Sentiment-Analysis-Dataset.csv") as csvfile:
+			data = csv.reader(csvfile) # 1578615 
+			next(data, None) # skip headers
+			# format ItemID, Sentiment, SentimentSource, SentimentText
+			# SentimentSource set is ['Sentiment140', 'Kaggle'] - not sure if it matters
+			counter = 0
+			correct = 0
+			for row in data:
+				
+				index = int(row[0])
+				if index % 10000 == 0:
+					print index
+				text = row[3]
+				polarity = int(row[1])
+				polarity_index = polarity if polarity==1 else -1
+				if index < train_limit:
+					try:
+						words = word_tokenize(text)
+						for w in words:
+							if w in self.dist:
+								self.dist[w] += polarity_index
+							else: 
+								self.dist[w] = polarity_index
+					except UnicodeDecodeError:
+						pass
+				elif index < test_limit:
+					predicted = self.get_polarity(text)
+					if predicted == polarity:
+						correct += 1
+					continue
+				else:
+					print correct * 1.0/(test_limit - train_limit) * 100 , "%"
+					return
+
