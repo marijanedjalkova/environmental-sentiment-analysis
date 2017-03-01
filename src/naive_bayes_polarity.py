39cfe1d8c3ff05deff1dbac7e5b43209c2a49996
marijanedjalkova <nedyalkova.marya@gmail.com>
2017-03-01 23:11:11 +0000

    extracting mentions and urls

--- src/naive_bayes_polarity.py
+++ src/naive_bayes_polarity.py
@@ -2,6 +2,7 @@ from nltk import word_tokenize
 from nltk.tokenize import TweetTokenizer
 import csv
 from textblob.classifiers import NaiveBayesClassifier
+import re
 
 class NBClassifier:
 
@@ -10,8 +11,18 @@ class NBClassifier:
 		self.train_limit = train_length
 		self.test_limit = train_length + test_length
                 self.tokenizer = TweetTokenizer()
-		self.train()
+                self.url_pattern = re.compile("(?P<url>https?://[^\s]+)")
+                self.train()
 
+        def preprocess_tweet(self, text):
+            decoded = text.decode("utf-8")
+            tokens = self.tokenizer.tokenize(decoded)
+            for tok in tokens:
+                if tok.startswith('@') and len(tok)>1:#not the 'i am @ the bar' cases
+                    tokens.remove(tok)
+                if self.url_pattern.match(tok):
+                    tokens.remove(tok)
+            return tokens
 
 
 	def train(self):
@@ -30,8 +41,7 @@ class NBClassifier:
 				        index = int(row[0])
 				        if index % 10000 == 0:
 					        print index
-				        text = (row[3]).decode("utf-8")
-                                        tokens = self.tokenizer.tokenize(text)
+                                        tokens = self.preprocess_tweet(row[3])
 				        polarity = int(row[1]) # 0 or 1
 				        if index < self.train_limit:
 					        training_data.append((tokens, polarity))
