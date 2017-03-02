6b6f73ba4eec945d901f8941ede4205e81de36c1
marijanedjalkova <nedyalkova.marya@gmail.com>
2017-03-02 01:13:13 +0000

    extracted classifier tests into a separate module

--- src/main.py
+++ src/main.py
@@ -2,7 +2,7 @@ from twitter_tool import TwitterTool
 from textBlob_classifier import TextBlobClassifier
 from very_naive_classifier import SimpleClassifier
 from naive_bayes_polarity import NBClassifier
-
+from dataset_analysis import *
 
 def test_twitter():
 	tt = TwitterTool()
@@ -13,19 +13,20 @@ def test_twitter():
 		print tweet.created_at, '\n',  tweet.text
 		print "--------------"
 
+def test_classifiers():
+	s = SimpleClassifier(500000, 1000)
+	acc = s.train_and_test()
+	print acc
+	s2 = TextBlobClassifier()
+	acc2 = s2.test()
+	s3 = NBClassifier(3500, 100)
+	acc3 = s3.test()
+	print "Simple: ", acc
+	print "TextBlob: ", acc2
+	print "Naive Bayes: ", acc3
 
 def main():
-	 s = SimpleClassifier(500000, 1000)
-	 acc = s.train_and_test()
-	 print acc
-	# s2 = TextBlobClassifier()
-	# acc2 = s2.test()
-	#s3 = NBClassifier()
-	#acc3 = s3.test()
-	#print "Simple: ", acc
-	#print "TextBlob: ", acc2
-	#print "Naive Bayes: ", acc3
-
+    print "hello world"
 
 if __name__ == '__main__':
 	main()
