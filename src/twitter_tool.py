bb8e0b51079c43b064d2c0ddca7dcdb3b77bf9d7
marijanedjalkova <nedyalkova.marya@gmail.com>
2017-03-16 19:27:27 +0000

    comparing to Testblob's and sentiment140 results

--- src/twitter_tool.py
+++ src/twitter_tool.py
@@ -2,6 +2,8 @@ import tweepy
 import json
 from stream_listener import StreamListener
 from ngram_classifier import Ngram_Classifier
+from senti140test import *
+from textblob import TextBlob
 
 class TwitterTool:
 
@@ -40,6 +42,14 @@ class TwitterTool:
 	def extract_text_from_tweets(self, tweets):
 		return [t.text for t in tweets if t.lang=="en"]
 
+	def get_textblob_polarity(self, tweet_text, neg_value, pos_value):
+		p = TextBlob(tweet_text).sentiment.polarity
+		if p < 0.0:
+			return neg_value, p
+		elif p > 0.0:
+			return pos_value, p
+		return (neg_value + pos_value) * 1.0/2, 0.0
+
 def main():
 	nc1 = Ngram_Classifier("SVM", 1, 300000, 0, "ngram_extractor")
 	training, testing = nc1.get_train_test_sets()
@@ -49,16 +59,35 @@ def main():
 	tt = TwitterTool()
 	tweets = tt.search_tweets("grangemouth", 100)
 	tweet_list = tt.extract_text_from_tweets(tweets)
-
+	correctCount = 0
+	overallCount = 0
+	neutralCount = 0
+	errors = 0
+	notes = []
 	for t in tweet_list:
 		if not t.startswith("RT"):
-			r1 = nc1.classify_one(t)
-			# r2 = nc2.classify_one(t)
-			# r3 = nc3.classify_one(t)
-			# r4 = nc4.classify_one(t)
+			r1 = float(nc1.classify_one(t))
+			overallCount += 1
+			sentiR, sentiAbsolute = tt.get_textblob_polarity(t, 0.0, 1.0)
+			sent140 = get_single_polarity(nc1.preprocess_tweet(nc1.decode_text(t)), 0.0, 1.0)
+			if sent140 ==-2:
+				errors += 1
+				continue
+			if r1 == sent140:
+				correctCount += 1
+			elif abs(r1 - sent140) <= 0.5:
+				neutralCount += 1
+			else:
+				notes += [(r1, sent140, t)]
 			print "Tweet: ", t 
-			print r1#, " vs ", r2 #, " vs ", r3, " vs ", r4
+			print r1, " vs ", sentiR , " <- ", sentiAbsolute, " vs ", sent140
 			print "========================================================="
+	print "Overall: ", overallCount
+	print "Correct: ", correctCount
+	print "Precision: ", correctCount * 100.0/overallCount, "%"
+	print "About right: ", neutralCount
+	print "Erorrs: ", errors
+	for n in notes: print n
 
 
 if __name__ == '__main__':
