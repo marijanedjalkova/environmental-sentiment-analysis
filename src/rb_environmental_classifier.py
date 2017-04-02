3cd53f896d43cd745d799af6290d859eca919957
marijanedjalkova <nedyalkova.marya@gmail.com>
2017-04-02 23:32:26 +0100

    classification

--- src/rb_environmental_classifier.py
+++ src/rb_environmental_classifier.py
@@ -10,3 +10,15 @@ NOUNS = ['smoke', 'flare', 'smog', 'fume', 'dust', 'asthma', 'idling', 'burn', '
 VERBS = ['smell', 'stink', 'reek']
 
 ADJECTIVES = ['gross', 'harmful', 'horrible', 'nasty', 'vile', 'foul']
+
+class RB_classifier(object):
+
+	def __init__(self):
+		pass 
+
+	def classify(self, text):
+		# decode
+		# tokenize 
+		# get rid of garbage 
+		# analyse 
+		return res
