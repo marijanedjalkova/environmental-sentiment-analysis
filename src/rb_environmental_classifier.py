729642cc06a35308adb4724da08adfcb96ad2ced
marijanedjalkova <nedyalkova.marya@gmail.com>
2017-04-02 21:39:31 +0100

    added the rest of the vocabulary

--- src/rb_environmental_classifier.py
+++ src/rb_environmental_classifier.py
@@ -1,1 +1,12 @@
 MENTIONS = ['falkirkcouncil', '^INEOS(_([^\s]+))?$', '^BP(_([^\s]+))?$', 'ICI', 'ScottishEPA']
+
+NAMES = ['ineos', 'petroineos', 'bp', 'calachem', 'ici', 'falkirk council', 'sepa']
+
+NOUNS = ['smoke', 'flare', 'smog', 'fume', 'dust', 'asthma', 'idling', 'burn', 'fish', 'egg', 
+'smell', 'odour', 'fly', 'rodent', 'vermin', 'insect', 'pest', 'rat', 'mouse', 'cockroach',
+'dirt', 'rubbish', 'dump', 'landfill', 'litter', 'fly-tip', 'disgust', 'danger', 'health',
+'harm', 'pollution', 'oil', 'sewage', 'hydrocarbons', 'flood', 'environment']
+
+VERBS = ['smell', 'stink', 'reek']
+
+ADJECTIVES = ['gross', 'harmful', 'horrible', 'nasty', 'vile', 'foul']
