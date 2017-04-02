1e26de3ea8dd7ce9ef23d7eb14b83c489dc5f9b2
marijanedjalkova <nedyalkova.marya@gmail.com>
2017-04-03 00:09:08 +0100

    recognizing mentions

--- src/rb_environmental_classifier.py
+++ src/rb_environmental_classifier.py
@@ -1,3 +1,7 @@
+import preprocessor 
+from nltk.tokenize import TweetTokenizer
+import re
+
 MENTIONS = ['falkirkcouncil', '^INEOS(_([^\s]+))?$', '^BP(_([^\s]+))?$', 'ICI', 'ScottishEPA']
 
 NAMES = ['ineos', 'petroineos', 'bp', 'calachem', 'ici', 'falkirk council', 'sepa']
@@ -11,6 +15,16 @@ VERBS = ['smell', 'stink', 'reek']
 
 ADJECTIVES = ['gross', 'harmful', 'horrible', 'nasty', 'vile', 'foul']
 
+BINGO = 1
+MISS = 0
+
+def is_in_mentions(mention):
+	for m in MENTIONS:
+		if re.match(m, mention):
+			return True
+	return False
+
+
 class RB_classifier(object):
 
 	def __init__(self):
@@ -18,7 +32,35 @@ class RB_classifier(object):
 
 	def classify(self, text):
 		# decode
+		text = text.encode('utf8')
 		# tokenize 
+		some_result = self.parsing(text)
+		tokens = preprocessor.tokenize()
 		# get rid of garbage 
 		# analyse 
 		return res
+
+	def parsing(self, text):
+		try:
+			parsed = preprocessor.parse(text)
+			if parsed.urls:
+				 pass
+			if parsed.mentions:
+				for m in parsed.mentions:
+					if is_in_mentions(m.match):
+						pass
+						# well, this should increase the chances by a loooooooot
+			if parsed.emojis:
+				pass
+		except (UnicodeDecodeError, UnicodeEncodeError) as e:
+			return 0
+
+if __name__ == '__main__':
+	print is_in_mentions("INEOS")
+	print is_in_mentions("INEOS_abc")
+	print is_in_mentions("INEOS_")
+	print is_in_mentions("blaneos")
+	print is_in_mentions("ici")
+	print is_in_mentions("ICI")
+	print is_in_mentions("BP")
+	print is_in_mentions("BP_abc_efd")
