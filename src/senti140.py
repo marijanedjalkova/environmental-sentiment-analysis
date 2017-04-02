5a67e9a712e784e715bf54cc4effbc9535e1b6f0
marijanedjalkova <nedyalkova.marya@gmail.com>
2017-04-02 18:39:42 +0100

    removed printing, neutral to positive bias

--- src/senti140.py
+++ src/senti140.py
@@ -2,6 +2,7 @@ import urllib2
 import json
 
 def get_single_polarity(tweet_text, neg_value, pos_value):
+	""" Will return positive is not negative. """
 	data = {"data": [{"text": ""}]}
 	new_list = []
 	for it in tweet_text:
@@ -12,22 +13,20 @@ def get_single_polarity(tweet_text, neg_value, pos_value):
 	joined = "+".join(new_list) 
 	data["data"][0]["text"] = joined
 	req = 'http://www.sentiment140.com/api/classify?text=' + joined
-	print req
 	try:
 		response = urllib2.urlopen(req) 
 		page = response.read()
-		print "response is ", len(page), " characters long."
 		d = json.loads(page)
 	except:
 		return -2
-	print d
 	p = d["results"]["polarity"]
 	if p == 0: 
 		return neg_value
 	if p == 4:
 		return pos_value
 	# return something in the middle, can then compare in a more complicated way
-	return (neg_value + pos_value) * 1.0 / 2
+	#return (neg_value + pos_value) * 1.0 / 2
+	return pos_value
 
 
 if __name__ == '__main__':
