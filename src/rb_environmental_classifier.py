d9f6e6535a35f5ac03552eb6946c08d4735d4863
marijanedjalkova <nedyalkova.marya@gmail.com>
2017-04-03 00:24:36 +0100

    check lexicon, names moved to mentions

--- src/rb_environmental_classifier.py
+++ src/rb_environmental_classifier.py
@@ -18,28 +18,7 @@ ADJECTIVES = ['gross', 'harmful', 'horrible', 'nasty', 'vile', 'foul']
 BINGO = 1
 MISS = 0
 
-def is_in_mentions(mention):
-	for m in MENTIONS:
-		if re.match(m, mention):
-			return True
-	return False
 
-def is_in_words(word):
-	if word in NOUNS:
-		return True 
-	if word in VERBS:
-		return True 
-	if word in ADJECTIVES:
-		return True
-	if check_other_forms(word):
-		return True 
-	return False
-
-def check_other_forms(word):
-	# check other forms of the same word
-
-def is_is_names(word):
-	return word in NAMES
 
 
 class RB_classifier(object):
@@ -56,25 +35,60 @@ class RB_classifier(object):
 		text = text.encode('utf8')
 		# tokenize 
 		some_result = self.parsing(text)
-		tokens = preprocessor.tokenize()
+		preprocessor.set_options(preprocessor.OPT.URL, preprocessor.OPT.EMOJI, preprocessor.OPT.MENTION)
+		cleaned = preprocessor.clean(text)
+		tokens = TweetTokenizer().tokenize(cleaned)
 		# get rid of garbage 
+		word_res = self.check_words(tokens)
 		# analyse 
 		return res
+
+	def check_words(self, tokens):
+		valence = 0
+		for t in tokens:
+			if self.is_in_lexicon(t):
+				# increase by a little 
+				valence += 0.2
+				continue 
+		return valence
 
 	def parsing(self, text):
 		try:
 			parsed = preprocessor.parse(text)
 			if parsed.urls:
-				 pass
+				pass
 			if parsed.mentions:
 				for m in parsed.mentions:
-					if is_in_mentions(m.match):
+					if self.is_in_mentions(m.match):
 						pass
 						# well, this should increase the chances by a loooooooot
 			if parsed.emojis:
 				pass
 		except (UnicodeDecodeError, UnicodeEncodeError) as e:
 			return 0
+
+	def is_in_mentions(self, mention):
+		for m in MENTIONS:
+			if re.match(m, mention):
+				return True
+		if mention in NAMES:
+			return True
+		return False
+
+	def is_in_lexicon(self, word):
+		if word in NOUNS:
+			return True 
+		if word in VERBS:
+			return True 
+		if word in ADJECTIVES:
+			return True
+		if self.check_lexicon(word):
+			return True 
+		return False
+
+	def check_lexicon(self, word):
+		# check other forms of the same word
+		return word in self.lexicon
 
 if __name__ == '__main__':
 	print is_in_mentions("INEOS")
