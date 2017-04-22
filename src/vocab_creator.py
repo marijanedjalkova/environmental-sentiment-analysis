64de6994e3877646e9d06411eb9ad715d09e1684
marijanedjalkova <nedyalkova.marya@gmail.com>
2017-04-22 18:32:17 +0100

    helper methods

--- src/vocab_creator.py
+++ src/vocab_creator.py
@@ -1,6 +1,14 @@
 import csv
 
-entities = csv.reader(open('/cs/home/mn39/Documents/MSciDissertation/resources/relatedEntities.csv'), delimiter = ',')
-filtered = [entity for entity in entities if len(entity)>1]
-cleaned = [name for [name,_] in filtered]
-print cleaned
+
+def read_csv_into_list(filename):
+	entities = csv.reader(open(filename), delimiter = ',')
+	filtered = [entity for entity in entities if len(entity)>1]
+	cleaned = [name for [name,_] in filtered]
+	print cleaned
+
+def main():
+	l1 = read_csv_into_list('/cs/home/mn39/Documents/MSciDissertation/resources/relatedEntities.csv')
+
+if __name__ == '__main__':
+	main()
