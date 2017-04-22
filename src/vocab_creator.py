78a009645b087c8a1791b9cb6b0e57e094458bed
marijanedjalkova <nedyalkova.marya@gmail.com>
2017-04-22 18:52:47 +0100

    reading from a folder of sources

--- src/vocab_creator.py
+++ src/vocab_creator.py
@@ -1,14 +1,27 @@
 import csv
+import os
+import glob
 
 
 def read_csv_into_list(filename):
-	entities = csv.reader(open(filename), delimiter = ',')
-	filtered = [entity for entity in entities if len(entity)>1]
+	rows = csv.reader(open(filename), delimiter = ',')
+	filtered = [row for row in rows if len(row)==2]
 	cleaned = [name for [name,_] in filtered]
-	print cleaned
+	return cleaned
+
+def read_all_sources(path):
+	extension = 'csv'
+	os.chdir(path)
+	vocab = []
+	source_files = [i for i in glob.glob('*.{}'.format(extension))]
+	for s in source_files:
+		vocab.extend(read_csv_into_list(s))
+	return list(set(vocab))
+
 
 def main():
-	l1 = read_csv_into_list('/cs/home/mn39/Documents/MSciDissertation/resources/relatedEntities.csv')
+	l = read_all_sources('/cs/home/mn39/Documents/MSciDissertation/resources/vocab_sources')
+	print len(l)
 
 if __name__ == '__main__':
 	main()
