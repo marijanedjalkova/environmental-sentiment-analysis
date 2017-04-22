0b31bbd0ab80cb934f38380962e433e6cff11d2b
marijanedjalkova <nedyalkova.marya@gmail.com>
2017-04-22 17:27:22 +0100

    added reading from file and writing to a file

--- src/elections.py
+++ src/elections.py
@@ -1,12 +1,28 @@
 from twitter_tool import * 
+import json
+
 
 
 def main():
+	read_from_file()
+
+def read_from_file():
+	with open('elections.txt') as json_data:
+		d = json.load(json_data)
+	print(len(d))
+
+def get_save_tweets():
 	tt = TwitterTool()
 	tweets = tt.search_tweets("election", 1500)
 	tweet_list = tt.extract_text_from_tweets(tweets)
-	length = len(tweet_list)
-	print len(set(tweet_list))==length
+	data = []
+	for tw in set(tweet_list):
+		dct = {}
+		dct["label"] = 0
+		dct["text"] = tw
+		data.append(dct)
+	with open('elections2.txt', 'w') as outfile:
+		json.dump(data, outfile)
 	
 
 if __name__ == '__main__':
