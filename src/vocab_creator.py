9c2e152c54f05269b1f3adcd519cf457b376661e
marijanedjalkova <nedyalkova.marya@gmail.com>
2017-04-23 15:24:48 +0100

    refactoring, comments

--- src/vocab_creator.py
+++ src/vocab_creator.py
@@ -4,6 +4,7 @@ import glob
 
 
 def read_csv_into_list(filename):
+	""" Read the Google-provided files into list of concepts """
 	rows = csv.reader(open(filename), delimiter = ',')
 	filtered = [row for row in rows if len(row)==2]
 	cleaned = [name for [name,_] in filtered]
@@ -23,10 +24,16 @@ def write_to_file(filename, data):
 		wr = csv.writer(output)
 		wr.writerows([data])
 
-
-def main():
+def create_local_vocab():
+	""" This should be done once. Then the resulting vocab should be modified manually."""
 	concepts = read_all_sources('/cs/home/mn39/Documents/MSciDissertation/resources/vocab_sources')
 	write_to_file('/cs/home/mn39/Documents/MSciDissertation/resources/vocab.txt', concepts)
+
+def read_in_local_vocab(filename):
+	
+
+def main():
+	create_local_vocab()
 
 if __name__ == '__main__':
 	main()
