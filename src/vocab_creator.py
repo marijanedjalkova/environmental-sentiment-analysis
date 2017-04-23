f18c72eb85198a47d89ec479fa17bd44f47682cd
marijanedjalkova <nedyalkova.marya@gmail.com>
2017-04-23 16:56:13 +0100

    class, constructing vocab

--- src/vocab_creator.py
+++ src/vocab_creator.py
@@ -2,6 +2,13 @@ import csv
 import os
 import glob
 import json
+import preprocessor 
+from nltk.tokenize import TweetTokenizer
+from nltk.corpus import wordnet
+from nltk.stem.wordnet import WordNetLemmatizer
+import re
+import urllib2
+from urlparse import urlparse
 
 class HeadRequest(urllib2.Request):
 	def get_method(self): return "HEAD"
@@ -11,41 +18,78 @@ def get_real_url(url):
 	res = urllib2.urlopen(HeadRequest(url))
 	return res.geturl()
 
-
-def read_csv_into_list(filename):
-	""" Read the Google-provided files into list of concepts """
-	rows = csv.reader(open(filename), delimiter = ',')
-	filtered = [row for row in rows if len(row)==2]
-	cleaned = [name for [name,_] in filtered]
-	return cleaned
-
-def read_all_sources(path):
-	extension = 'csv'
-	os.chdir(path)
-	vocab = []
-	source_files = [i for i in glob.glob('*.{}'.format(extension))]
-	for s in source_files:
-		vocab.extend(read_csv_into_list(s))
-	return list(set(vocab))
-
-def write_to_file(filename, data):
+def write_to_csv(filename, data):
+	""" Writes given list of data to a specofoed file """
 	with open(filename, 'w') as output:
 		wr = csv.writer(output)
 		wr.writerows([data])
 
-def create_local_vocab():
-	""" This should be done once. Then the resulting vocab should be modified manually."""
-	concepts = read_all_sources('/cs/home/mn39/Documents/MSciDissertation/resources/vocab_sources')
-	write_to_file('/cs/home/mn39/Documents/MSciDissertation/resources/election_vocab.txt', concepts)
-
-def read_in_local_vocab(filename):
+def read_in_json(self, filename):
+	""" Reads in json from a file """
 	with open(filename) as json_data:
 		d = json.load(json_data)
 	return d 
 
+class VocabBuilder():
+
+	def __init__(self):
+		self.wnl = WordNetLemmatizer()
+
+	def read_csv_into_list(self, filename):
+		""" Read the Google-provided file into list of concepts """
+		rows = csv.reader(open(filename), delimiter = ',')
+		filtered = [row for row in rows if len(row)==2]
+		cleaned = [name for [name,_] in filtered]
+		return cleaned
+
+	def read_all_sources(self, path):
+		""" Reads all Google-provided csvs from a folder and returns a list or unique words. """
+		extension = 'csv'
+		os.chdir(path)
+		vocab = []
+		source_files = [i for i in glob.glob('*.{}'.format(extension))]
+		for s in source_files:
+			vocab.extend(self.read_csv_into_list(s))
+		return list(set(vocab))
+
+	def create_local_vocab(self):
+		""" This should be done once. Then the resulting vocab should be modified manually."""
+		concepts = self.read_all_sources('/cs/home/mn39/Documents/MSciDissertation/resources/vocab_sources')
+		write_to_csv('/cs/home/mn39/Documents/MSciDissertation/resources/election_vocab.txt', concepts)
+
+
+
+	def _initialise_lexicon(self, vocab):
+			""" Finds other forms of the same words and their synonyms and adds to the lexicon """
+			stems = set()
+			for w in vocab['nouns']:
+				stem = self.wnl.lemmatize(w)
+				stems.add(stem)
+				synonyms = self._get_synonyms(w)
+				for s in synonyms:
+					if s == "postmortem_examination":
+						print w, " - > postmortem_examination"
+					stem = self.wnl.lemmatize(s)
+					stems.add(stem)
+			return stems
+
+	def _get_synonyms(self, word):
+		""" Returns a list of synonyms of a word """
+		syns = wordnet.synsets(word)
+		return [l.name().encode('utf8') for s in syns for l in s.lemmas()]
+
+	def construct_vocab(self):
+		vocab_raw = self.read_in_json('/cs/home/mn39/Documents/MSciDissertation/resources/election_vocab.txt')
+		vocab_more = self._initialise_lexicon(vocab_raw)
+		vocab_raw['stems'] = vocab_more
+		self.vocab = vocab_raw
+
+
 def main():
-	vocab_raw = read_in_local_vocab('/cs/home/mn39/Documents/MSciDissertation/resources/election_vocab.txt')
-	print vocab_raw.keys()
+	vb = VocabBuilder()
+	vb.construct_vocab()
+	
+	
 
 if __name__ == '__main__':
 	main()
