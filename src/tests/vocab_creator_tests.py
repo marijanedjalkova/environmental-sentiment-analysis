8faf7825bf1df6cd626ae5f715e20a3e57a4312a
marijanedjalkova <nedyalkova.marya@gmail.com>
2017-04-27 15:50:43 +0100

    test initialisation

--- src/tests/vocab_creator_tests.py
+++ src/tests/vocab_creator_tests.py
@@ -8,15 +8,26 @@ class TestVocabCreator(unittest.TestCase):
 
 	def setUp(self):
 		""" Run before every test"""
-		print "setup"
 		self.vb = VocabBuilder()
 
 	def test_constructor(self):
-		self.assertNotEqual(self.vb.wnl, None)
+		self.assertIsNotNone(self.vb.wnl)
 
-	def test_lower(self):
-		self.assertEqual('foo'.upper(), 'FOO')
+	def test_read_csv_into_list(self):
+		res = self.vb.read_csv_into_list('/cs/home/mn39/Documents/MSciDissertation/resources/vocab_sources/relatedEntities.csv')
+		self.assertIsNotNone(res)
 
+	def test_all_sources_to_set(self):
+		res = self.vb.all_sources_to_set('/cs/home/mn39/Documents/MSciDissertation/resources/vocab_sources')
+		self.assertIsNotNone(res)
+
+	def test_initialise_lexicon(self):
+		stems = self.vb._initialise_lexicon({})
+		self.assertTrue(not stems)
+		stems = self.vb._initialise_lexicon({'nouns':[]})
+		self.assertTrue(not stems)
+		stems = self.vb._initialise_lexicon({'nouns':['one','two','three']})
+		self.assertFalse(not stems)
 
 
 
