a5599ffda6fdf8b28beab6ca38373b541eddf3c4
marijanedjalkova <nedyalkova.marya@gmail.com>
2017-04-28 16:22:43 +0100

    textblob polarity test

--- src/tests/twitter_tool_tests.py
+++ src/tests/twitter_tool_tests.py
@@ -13,5 +13,12 @@ class TestTwitterTool(unittest.TestCase):
 	def test_constructor(self):
 		self.assertIsNotNone(self.tt.api)
 
+	def test_get_textblob_polarity(self):
+		r = self.tt.get_textblob_polarity("OMG I am so so happy!", 0, 1)
+		self.assertIsNotNone(r)
+		self.assertIsInstance(r, tuple)
+		self.assertTrue(r[0]>=0)
+		self.assertTrue(r[0]<=1)
+
 if __name__ == '__main__':
     unittest.main()
