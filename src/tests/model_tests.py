58a2060ca34c670ffe318e2dafd1817bb4fb43a2
marijanedjalkova <nedyalkova.marya@gmail.com>
2017-04-29 22:47:05 +0100

    tests for classification and data setting

--- src/tests/model_tests.py
+++ src/tests/model_tests.py
@@ -17,11 +17,26 @@ class TestElections(unittest.TestCase):
 
 	def test_get_data_chunks(self):
 		ch = list(self.tm.get_data_chunks(0))
-		self.assertEquals(len(ch),1)
+		self.assertEquals(len(ch), 1)
 		ch = list(self.tm.get_data_chunks(1))
-		self.assertEquals(len(ch),len(self.tm.data))
+		self.assertEquals(len(ch), len(self.tm.data))
 
+	def test_set_training_testing_data(self):
+		self.tm.set_training_testing_data(0)
+		self.assertEquals(len(self.tm.testing_data), len(self.tm.data)*0.1)
+		self.assertEquals(len(self.tm.training_data), len(self.tm.data)*0.9)
+		self.tm.set_training_testing_data(1)
+		self.assertEquals(len(self.tm.testing_data), len(self.tm.data)*0.1)
+		self.assertEquals(len(self.tm.training_data), len(self.tm.data)*0.9)
+		self.tm.set_training_testing_data(0.5)
+		self.assertEquals(len(self.tm.testing_data), len(self.tm.training_data))
 
+	def test_classify(self):
+		self.tm.set_classifier()
+		c, v = self.tm.classify(unicode("@masha is writing her #dissertation #cslife http://amazing.net"))
+		self.assertIsInstance(c, int)
+		self.assertIsInstance(v, dict)
+		
 
 if __name__ == '__main__':
     unittest.main()
